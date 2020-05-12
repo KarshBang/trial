e1df80af065ea49a5fff7e12cd746682732636ce
KarshBang <wswswzb@hotmail.com>
2020-05-12 18:41:30 +0800

    Updates

---
+++ precache-manifest.b547c26a92d9ffc142ee17a74c997c86.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba75ec9581017b82731e1f94382e9f7b",
+    "url": "/trial/index.html"
+  },
+  {
+    "revision": "83fe6e081b3633aa45f4",
+    "url": "/trial/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "940f8fa010b116ac73dd",
+    "url": "/trial/static/js/2.08bf8eba.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/trial/static/js/2.08bf8eba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83fe6e081b3633aa45f4",
+    "url": "/trial/static/js/main.02da0fbc.chunk.js"
+  },
+  {
+    "revision": "0634193b3233a5954e65",
+    "url": "/trial/static/js/runtime-main.e1c6af14.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/trial/static/media/logo.5d5d9eef.svg"
+  }
+]);
