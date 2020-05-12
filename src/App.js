0be4f3890fb88ee8879014e95b9589dfb7a4ede2
KarshBang <wswswzb@hotmail.com>
2020-05-12 18:39:43 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
+import logo from './logo.svg'
+import './App.css'
+
+function Index() {
+  return (
+    <h1>gitpage trial</h1>
+  )
+}
+
+
+function Hello() {
+  return <p>Hello World</p>
+}
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router basename={process.env.PUBLIC_URL}>
+      <ul>
+        <li><Link to='/'>home</Link></li>
+        <li><Link to='/hello/'>hello world</Link></li>
+      </ul>
+      <Route path='/' exact component={Index}></Route>
+      <Route path='/hello/' component={Hello}></Route>
+    </Router>
+  )
 }
 
 export default App;
